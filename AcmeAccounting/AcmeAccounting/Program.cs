93aefcae843e036ed4ff11eeffc3d9203344f273
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-03-31 14:37:16 -0700

    Add Acme Accounting practice to repo

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+    }
+    }
