d12e2b0bf163a7ef0b85e5427b2643974004af70
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-03 22:30:36 -0700

    Add car insurance app to repo

---
+++ CarInsuranceApp/CarInsuranceApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CarInsuranceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            bool age = (userAge >= 16);
+
+            Console.WriteLine("Have you ever had a DUI?" + "\nPlease enter true or false only");
+            string drunk = Console.ReadLine();
+            Console.WriteLine();
+            bool DUI = Convert.ToBoolean(drunk);
+            Console.WriteLine("Press Enter to continue");
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press Enter to continue");
+            bool speed = (ticket <= 3);
+
+            bool result = (age && speed == true && DUI == false);
+            Console.WriteLine("Qualified?" + "\n" + result);
+            Console.ReadLine();
+        }
+    }
+}
