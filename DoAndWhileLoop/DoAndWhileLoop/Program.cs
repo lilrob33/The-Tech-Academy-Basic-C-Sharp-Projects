3cb8fb79697f4b6d2c1be9ec757b5a74e16f7f33
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-05 02:42:43 -0700

    Add do while loop to repo

---
+++ DoAndWhileLoop/DoAndWhileLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAndWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do while statement guess a number
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            do 
+            {
+                Console.WriteLine(number);
+                number++;
+                // will icrement number if input under 20 to 20
+            } while (number < 20);
+
+            Console.ReadLine();
+
+            // Continue in while-loop until index is equal to 10.
+            int i = 0;
+            while (i < 10)
+            {
+                Console.Write("While statement ");
+                Console.WriteLine(i);
+                // Increment the variable.
+                i++;
+            }
+            Console.ReadLine();
+                
+        }
+    }
+}
