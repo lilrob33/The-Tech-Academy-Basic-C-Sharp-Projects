86737d0091c9974ba2b0714e4a20b05d0eb4bf9a
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-04 18:32:22 -0700

    Add if/else to file

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -17,43 +17,72 @@ namespace PackageExpress
             // Takes the weight of the package from the user and stores in variable for the quote
             Console.WriteLine("Please enter the package weight:");
             weight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Press enter to continue");
-            Console.ReadLine();
-
-            // Takes the width of the package from the user and stores in variable for the quote
-            Console.WriteLine("Please enter the package width:");
-            width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Press enter to continue");
-            Console.ReadLine();
-
-            // Takes the height of the package from the user and stores in variable for the quote
-            Console.WriteLine("Please enter the package height:");
-            height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Press enter to continue");
-            Console.ReadLine();
-
-            // Takes the length of the package from the user and stores in variable for the quote
-            Console.WriteLine("Please enter the package length:");
-            length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Press enter to continue");
-            Console.ReadLine();
-
-            // Creates a quote from variables from user inputs
-            quote = (weight * width * height * length);
-            finQuote = Convert.ToDecimal(quote) / 100m;
-
-            // if and else, If the dimensions total greater than 50, display the error message 
-            if (weight > 50 || width > 50 || height > 50 || length > 50)
+            if (weight > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
                 Console.ReadLine();
+                Environment.Exit(0);
             }
 
             else
             {
-                Console.WriteLine("Your estimated total for shipping this package is:" + "\n$" + finQuote + "\nThank you.");
+                Console.WriteLine("Please press Enter to continue");
                 Console.ReadLine();
             }
+            // Takes the width of the package from the user and stores in variable for the quote
+            Console.WriteLine("Please enter the package width:");
+            width = Convert.ToInt32(Console.ReadLine());
+            if (width > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("Please press Enter to continue");
+                Console.ReadLine();
+            }
+
+            // Takes the height of the package from the user and stores in variable for the quote
+            Console.WriteLine("Please enter the package height:");
+            height = Convert.ToInt32(Console.ReadLine());
+            if (height > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("Please press Enter to continue");
+                Console.ReadLine();
+            }
+
+            // Takes the length of the package from the user and stores in variable for the quote
+            Console.WriteLine("Please enter the package length:");
+            length = Convert.ToInt32(Console.ReadLine());
+            if (length > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("Please press Enter to continue");
+                Console.ReadLine();
+            }
+           
+            // Creates a quote from variables from user inputs
+            quote = (weight * width * height * length);
+            finQuote = Convert.ToDecimal(quote) / 100m;
+
+            Console.WriteLine("Your estimated total for shipping this package is:" + "\n$" + finQuote + "\nThank you.");
+                Console.ReadLine();
         }
     }
 }
