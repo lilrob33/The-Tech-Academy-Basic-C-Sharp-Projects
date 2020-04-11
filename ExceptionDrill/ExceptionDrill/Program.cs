ad61f42919528843eebcf9f0cd77b7be55bb996f
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-10 23:07:01 -0700

    Add exception drill to repo

---
+++ ExceptionDrill/ExceptionDrill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            // set up array
+            int[] numArray = { 10, 200, 230, 78, 120 };
+            Console.WriteLine("Please pick a number to divide into the numbers in the list.");
+
+            try 
+            {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            foreach (int items in numArray) // foreach to iterate through array
+            {    
+                    Console.WriteLine(items + " divided by " + num1 + " = " + items / num1);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type numbers only: {0}", ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Unable to divide by zero: {0}", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+      
+        finally
+        {
+            Console.WriteLine("\nProgram has emerged from the try/catch block and execution to be continued...");
+            Console.ReadLine();
+        }
+        }
+    }
