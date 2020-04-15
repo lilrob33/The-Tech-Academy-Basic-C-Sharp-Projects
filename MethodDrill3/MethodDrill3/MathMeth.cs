d07543adb293abf072e5f9a3f1acffd1d779d410
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-14 20:01:16 -0700

    Add method drill 3 to repo

---
+++ MethodDrill3/MethodDrill3/MathMeth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    class MathMeth
+    {
+        public int Sum(int num1, int optionalNum = 4)
+        {
+            var Total = num1 + 25 + optionalNum;
+            return Total;
+        }
+    }
+}
---
+++ MethodDrill3/MethodDrill3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter two numbers for the math operation, one at a time." +
+                " No need to enter anything for the second number.");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                int total;
+
+                MathMeth n = new MathMeth();
+                total = n.Sum(a, b);
+                Console.WriteLine("The sum of " + a + " , " + 25 + " and " + b + " is " + total);
+                
+                total = n.Sum(a);
+                Console.WriteLine("The sum of " + a + " , " + 25 + " and " + "optional number 4 is " + total);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter a number", ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
