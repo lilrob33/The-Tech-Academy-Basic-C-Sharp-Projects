444b07efe3011606b92a35d66f71e073609ce666
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-14 17:39:53 -0700

    Add method drill to repo

---
+++ MethodDrill/MethodDrill/MathMeth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    public class MathOp
+    {
+        // 1. Create a class. In that class, create three methods, each of which 
+        // will take one integer parameter in and return an integer.The methods 
+        // should do some math operation on the received parameter.Put this 
+        // class in a separate.cs file in the application.
+        // public int Sum(int num1, int num2)
+        {
+            var Total = num1 + num2;
+            return Total;
+        }
+        public int Product(int num1, int num2)
+        {
+            var Total = num1 * num2;
+            return Total;
+        }
+        public int Difference(int num1, int num2)
+        {
+            var Total = num1 - num2;
+            return Total;
+        }
+    }
+}
---
+++ MethodDrill/MethodDrill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.In the Main() program, ask the user what number they want to do the 
+            // math operations on.
+           Console.WriteLine("Please enter an integer for the math operations.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 32;
+            int total;
+            MathOp n = new MathOp();
+
+            //3.Call each method in turn, passing the user input to the method.
+            //Display the returned integer to the screen.
+          total = n.Sum(a, b);
+            Console.WriteLine("The sum of " + a + " and " + b + " is " + total);
+            total = n.Product(a, b);
+            Console.WriteLine("The product of " + a + " and " + b + " is " + total);
+            total = n.Difference(a, b);
+            Console.WriteLine("The difference of " + a + " and " + b + " is " + total);
+            Console.ReadLine();
+
+        }
+    }
+}
