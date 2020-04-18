0a6eca35f7423dbd8dab80d1695a9467abb401c0
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-17 23:11:02 -0700

    Add files via upload

---
+++ MethodDrill4/MethodDrill4/MathMeth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill4
+{
+    class MathMeth
+    {
+        public int Sum(int a, int b = 10)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
