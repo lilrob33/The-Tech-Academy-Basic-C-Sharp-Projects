99f334958a33935807d04e3deac3553dd8ae2e09
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-17 22:20:01 -0700

    Add files via upload

---
+++ MethodDrill5/MethodDrill5/MathMeth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill5
+{
+    class MathMeth
+    {
+        public void Division(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine("Your number divided by 2 equals: " + result);
+            return;
+        }
+
+        public void Division(out int x, out int y)
+        {
+            Console.WriteLine("Select a number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select another number");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static int Print()
+        {
+            int d = 50;
+            return d;
+        }
+    }
+}
