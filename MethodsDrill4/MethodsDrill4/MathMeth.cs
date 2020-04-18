6f3219185de0d139a3ba6b05fc403d2a96b5a1b6
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-17 23:18:44 -0700

    Add files via upload

---
+++ MethodsDrill4/MethodsDrill4/MathMeth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill4
+{
+    class MathMeth
+    {
+            public static void Drill(int param1, int param2)
+            {
+                int result = param1 + 1;
+                Console.WriteLine(result);
+                Console.WriteLine(param2);
+                Console.ReadLine();
+            }
+    }
+}
---
+++ MethodsDrill4/MethodsDrill4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill4
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int num1 = 3;
+            int num2 = 5;
+            MathMeth.Drill(num1, num2);
+        }
+    }
+}
