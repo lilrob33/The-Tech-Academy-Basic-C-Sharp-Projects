e998c21fc8a541822499b2925aacafad0abe383c
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-19 10:26:05 -0700

    Add abstract to class line 9

--- Employee/Employee/Person.cs
+++ Employee/Employee/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Employee
 {
-    public class Person
+    public abstract class Person
     {
         public string firstName { get; set; }
         public string lastName { get; set; }
