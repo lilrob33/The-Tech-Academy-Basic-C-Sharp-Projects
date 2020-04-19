8005610b7650d69a45854dfd6c14ac0ecf69d47d
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-19 12:01:02 -0700

    Add quittable interface to employee repo

---
+++ Employee/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit()
+        {
+            IQuittable quittable = new Employee();
+            Console.WriteLine("You can take this job and shove it..... I QUIT!");
+            Console.ReadLine();
+        }
+        public int ID { get; set; }
+    }
+}
