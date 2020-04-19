349bdbb412f8b10abab7c5f7271ba3ab22718841
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-19 15:33:09 -0700

    Update employee comparison

---
+++ Employee/Employee/Employee/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employee : Person, IQuittable
+    {
+        public static bool operator ==(Employee employee, Employee employee1)
+        {
+            if (employee.ID == employee1.ID)
+            {
+                bool isSame = true;
+                return isSame;
+            }
+            else
+            {
+                bool isSame = false;
+                return isSame;
+            }
+        }
+
+        public static bool operator !=(Employee employee, Employee employee1)
+        {
+            if (employee.ID != employee1.ID)
+            {
+                bool isNotSame = true;
+                return isNotSame;
+            }
+            else
+            {
+                bool isNotSame = false;
+                return isNotSame;
+            }
+        }
+
+        public void Quit()
+        {
+            IQuittable quittable = new Employee();
+            Console.WriteLine("You can take this job and shove it..... I QUIT!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Employee/Employee/Employee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.ID = 1;
+
+            Employee employee1 = new Employee();
+            employee1.firstName = "Student";
+            employee1.lastName = "Sample";
+            employee1.ID = 2;
+
+            employee.sayName();
+            employee1.sayName();
+            Console.WriteLine("Does the Employee ID numbers Match?");
+            //employee.Quit();
+            Console.WriteLine(employee == employee1);
+            Console.ReadLine();
+        }
+    }
+}
