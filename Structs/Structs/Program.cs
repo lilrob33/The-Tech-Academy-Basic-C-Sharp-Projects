34fa1f7dbbebcfc0b1fccefe0052b5e43d235b16
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-22 17:45:33 -0700

    Add structs to repo

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n = new Number();
+            n.Amount = 3.75m;
+            Console.WriteLine(n.Amount);
+            Console.ReadLine();
+        }
+    }
+}
