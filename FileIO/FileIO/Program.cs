e0479a1c1d4a1427978ea64c6688305b2f8fb86c
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-27 19:47:03 -0700

    Add fileIO to repo

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            Console.WriteLine("Please enter any number");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\users\psuar\source\repos\FileIO\log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            Console.WriteLine("Your number is " + userInput);
+            Console.ReadLine();
+        }
+    }
+}
