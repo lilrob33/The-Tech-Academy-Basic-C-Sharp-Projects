9a8d53a77ca12b1c9ff9074552a14f4d8e8cdce2
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-27 20:22:27 -0700

    Add DateTimeDrill files to repo

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+
+            Console.WriteLine("Please enter a number and we will add that to the current time" +
+                " to see what time it will be.");
+            var hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime timeInHours = currentTime.AddHours(hours);
+            Console.WriteLine("It will be " + timeInHours + " in " + hours + " hours.");
+            Console.ReadLine();
+        }
+    }
+}
