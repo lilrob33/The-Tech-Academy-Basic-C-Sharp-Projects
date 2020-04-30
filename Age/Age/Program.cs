7f80230e5293ce39f069e14cf14d406fe1fac179
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-29 19:36:20 -0700

    Add age files to repo

---
+++ Age/Age/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validAnswer = false;
+            int age = 0;
+            while (!validAnswer)
+            {
+                try
+                {
+                    Console.WriteLine("How old will you be this year?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+                    else
+                    {
+                        DateTime now = DateTime.Now;
+                        int year = now.Year;
+                        int birthYear = year - age;
+                        Console.WriteLine("You were born in " + birthYear);
+                        Console.ReadLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+
+        }
+    }
+}
