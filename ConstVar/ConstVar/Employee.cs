1688c1124cba7bd23b81e09b90c556d01a40e52c
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-04-29 19:23:02 -0700

    Add ConstVar files to repo

---
+++ ConstVar/ConstVar/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Employee
+    {
+        public int id;
+        public string name;
+        public Employee() : this(0, "") { }
+        public Employee(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public Employee(int id) : this(id, "") { }
+        public Employee(string name) : this(0, name) { }
+    }
+}
---
+++ ConstVar/ConstVar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string Company = "Esco";
+            var Year = 1913;
+            Employee employee1 = new Employee(9026, "Pedro Suarez");
+            Employee employee2 = new Employee("Eric Estrada");
+            Console.WriteLine("Welcome to " + Company + ". \nEstablished in " + Year + ", we have two employees that can help you with your needs. \n" + employee1.name + " and " + employee2.name);
+            Console.ReadLine();
+        }
+    }
+}
